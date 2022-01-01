03d6870ed47cd59776916003e7130077c90fb68c
ac5599656 <ac5599656@gmail.com>
2021-12-31 22:36:14 -0700

    added conditions to show error for no results and to show results

--- index.php
+++ index.php
@@ -18,6 +18,14 @@ $sql = "SELECT *
 
 $results = mysqli_query($conn, $sql);
 
+if($results === false) {
+    echo mysqli_error($conn);
+} else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC)
+
+    var_dump($articles);
+}
+
 ?>
 
 <!DOCTYPE html>
