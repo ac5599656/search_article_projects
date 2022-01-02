a751847ed10691f0be2f175cd3f004938c19c922
ac5599656 <ac5599656@gmail.com>
2022-01-01 21:01:54 -0700

    created header.php and footer.php, so the beginning and the end of the html code is not repeating in the index.php and article.php

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php
 
-$db_host = "xxxxx";
-$db_name = "xxxxx";
-$db_user = "xxxx";
-$db_pass = "xxxxxxxxx";
-
-$conn = mysqli_connect($db_host,$db_user,$db_pass, $db_name);
-
-if(mysqli_connect_error()){
-    echo mysqli_connect_error();
-    exit;
-}
+require 'database.php';
 
 $sql = "SELECT *
          FROM article
@@ -27,19 +17,7 @@ if($results === false) {
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Anne's Blog</title>
-</head>
-<body>
-    <header>
-        <h1>Anne's Blog</h1>
-    </header>   
-    <main>
+<?php require 'header.php'; ?>
         <?php if (empty($articles)): ?>
             <p>No articles found.</p>
         <?php else: ?>
@@ -54,7 +32,5 @@ if($results === false) {
                 <?php endforeach; ?>
             </ul>
         <?php endif; ?>
-   </main>
 
-</body>
-</html>
+<?php require 'footer.php'; ?>
